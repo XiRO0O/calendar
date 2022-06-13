d5678d3fb250ccf3ef177cb18c36d9677a602106
ONKJUNUA <jaseko2002@gmail.com>
2022-06-13 16:59:02 +0200

    End version

--- code.py
+++ code.py
@@ -3,4 +3,8 @@ import calendar
 year = int(input('Enter a year: '))
 month = int(input('Enter a month: '))
 
-print(calendar.calendar(year, month))
+print()
+if month <= 12:
+    print(calendar.month(year, month))
+else:
+    print("This month doesn't exist!")
