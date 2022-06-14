97b8eb79ed256f9550a7335cd2f14a67449d5104
ONKJUNUA <jaseko2002@gmail.com>
2022-06-14 15:50:05 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,20 @@
 import calendar
 
 date = str(input('What are you looking for (month/year)? '))
+print()
 
 if date == 'month':
-    y = (input('Enter a year: '))
-    m = (input('Enter a month: '))
+
+    y = int(input('Enter a year: '))
+    m = int(input('Enter a month: '))
     print()
-    if m != int:
-        print('You need to choose number!')
-    elif m <= 12:
+    if m <= 12:
         print(calendar.month(y, m))
     else:
         print("This month doesn't exist!")
 
 elif date == 'year':
-    y = (input('Enter a year: '))
+    y = int(input('Enter a year: '))
     print()
     print(calendar.calendar(y))
 
